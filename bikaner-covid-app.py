8d9b4a4401cdd5c5fb24236f9d3854989c829eab
Vivek Dadhich <ivivekdadhich@gmail.com>
2021-06-20 00:20:12 +0530

    Update bikaner-covid-app.py

--- bikaner-covid-app.py
+++ bikaner-covid-app.py
@@ -9,25 +9,6 @@ import os
 import time
 import stat
 
-from gsheetsdb import connect
-
-# Create a connection object.
-conn = connect()
-
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    return rows
-
-sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
-
 df = pd.read_csv('Bikaner-covid-cases-data.csv')
 blankIndex=[''] * len(df)
 df.index=blankIndex
